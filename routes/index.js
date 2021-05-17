c3326396dff5b36fb1b22187fc9c21faa25bc12a
fernie-cpu <yoloisjustanick@gmail.com>
2021-05-17 19:40:54 -0300

    router.post() to push submitted messages

--- routes/index.js
+++ routes/index.js
@@ -29,4 +29,13 @@ router.get('/new', (req, res, next) => {
   res.render('form');
 });
 
+router.post('/new', (req, res, next) => {
+  messages.push({
+    text: req.body.message,
+    user: req.body.name,
+    added: new Date(),
+  });
+  res.redirect('/');
+});
+
 module.exports = router;
