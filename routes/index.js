94770e4703caa6825b0d09d8667c461c70b1ef8f
fernie-cpu <yoloisjustanick@gmail.com>
2021-05-17 19:22:22 -0300

    Create messages object and get routers

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: 'Hi there, Jeff!',
+    user: 'Briggs',
+    added: new Date(),
+  },
+  {
+    text: 'Doc girl! Hello 👋',
+    user: 'Tati',
+    added: new Date(),
+  },
+  {
+    text: 'Heil, Queen!',
+    user: 'Cat Queen',
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Mini Message Board', messages: messages });
+  next();
+});
+
+router.get('/new', (req, res, next) => {
+  res.render('form');
 });
 
 module.exports = router;
